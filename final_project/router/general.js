e8bfceeaedee557bb98dd439707d6d78acdf2b81
melvinlim83-droid <melvinlim83@gmail.com>
2026-01-30 08:41:24 -0500

    Wrapped existing route handlers using promise (task 10 - task 13)

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -24,11 +24,21 @@ public_users.post("/register", (req,res) => {
 });
 
 // Get the book list available in the shop
+/*
 public_users.get('/',function (req, res) {
   res.status(200).send(JSON.stringify(books, null, 4));
 });
+*/
+public_users.get('/',function (req, res) {
+    new Promise(resolve => {
+        setTimeout(() => resolve(books), 0);
+    })
+        .then(data => res.status(200).send(JSON.stringify(data, null, 4)))
+        .catch(error => res.status(500).json(error));
+});
 
 // Get book details based on ISBN
+/*
 public_users.get('/isbn/:isbn',function (req, res) {
     const isbn = req.params.isbn;
     let book = {};
@@ -37,8 +47,27 @@ public_users.get('/isbn/:isbn',function (req, res) {
     }
     res.status(200).send(book);
  });
+ */
+ public_users.get('/isbn/:isbn',function (req, res) {
+    new Promise((resolve, reject) => {
+        setTimeout(() => {
+            let book = {};
+            const isbn = req.params.isbn;
+            
+            if (isbn) {
+                book[isbn] = books[isbn];
+                resolve(book);
+            }
+
+            reject(new Error("Book is not found"));
+        })
+            .then(data => res.status(200).send(JSON.stringify(data, null, 4)))
+            .catch(error => res.status(500).json(error));
+    });
+ });
   
 // Get book details based on author
+/*
 public_users.get('/author/:author',function (req, res) {
     const author = req.params.author;
     let filteredBooks = {};
@@ -51,8 +80,27 @@ public_users.get('/author/:author',function (req, res) {
 
     res.status(200).send(filteredBooks);
 });
+*/
+public_users.get('/author/:author',function (req, res) {
+    new Promise((resolve) => {
+        setTimeout(() => {
+            const author = req.params.author;
+            let filteredBooks = {};
+        
+            for (const key in books) {
+                if(books[key].author === author) {
+                    filteredBooks[key] = books[key];
+                }
+            } 
+            resolve(filteredBooks);
+        }, 0);
+    })
+        .then(data => res.status(200).send(JSON.stringify(data, null, 4)))
+        .catch(error => res.status(500).json(error));
+});
 
 // Get all books based on title
+/*
 public_users.get('/title/:title',function (req, res) {
     const title = req.params.title;
     let filteredBooks = {};
@@ -64,6 +112,24 @@ public_users.get('/title/:title',function (req, res) {
     }
 
     res.status(200).send(filteredBooks);
+});
+*/
+public_users.get('/title/:title',function (req, res) {
+    new Promise(resolve => {
+        setTimeout(() => {
+            const title = req.params.title;
+            let filteredBooks = {}; 
+
+            for (const key in books) {
+                if(books[key].title === title) {
+                    filteredBooks[key] = books[key];
+                }
+            }
+            resolve(filteredBooks);
+        }, 0);
+    })
+        .then(data => res.status(200).send(JSON.stringify(data, null, 4)))
+        .catch(error => res.status(500).json(error));
 });
 
 //  Get book review
